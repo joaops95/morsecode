b392010524ec0955f9e69f95232baaf5845b4108
joao <joao@mutablep.com>
2023-03-29 20:11:22 +0100

    dev

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+import logging
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+# Configure logging to stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+@app.route('/', methods=['GET', 'POST'])
 def log_data():
-    data = request.get_data(as_text=True)
-    print(f"Received data: {data}")
-    return "Data received!"
+    if request.method == 'GET':
+        logging.info(f"Received GET request: {request}")
+        return "Hello, World!"
+    elif request.method == 'POST':
+        data = request.get_data(as_text=True)
+        logging.info(f"Received POST request: {request}, data: {data}")
+        return "Data received!"
+    else:
+        logging.warning(f"Received request with unsupported HTTP method: {request}")
+        return "Method not allowed!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
